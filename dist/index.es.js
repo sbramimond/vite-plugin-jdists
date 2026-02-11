d7de6aab175e4494bd41bf09d8f13828fe538cfa
neverland <enix@foxmail.com>
2026-02-11 15:18:02 +0800

    feat: support esm

--- dist/index.es.js
+++ dist/index.es.js
@@ -1,55 +1,33 @@
-import g from "jdists";
-import { createFilter as m } from "@rollup/pluginutils";
-function j(f = {}) {
-  const {
-    include: c = /\.(js|ts|jsx|tsx|vue|svelte|astro|md)$/,
-    exclude: a = [],
-    remove: i = ["debug", "test"],
-    trigger: n = ["release"],
-    config: s,
-    ...d
-  } = f, o = m(c, a), l = (r) => Array.isArray(r) ? r.join(",") : r;
-  return {
-    name: "vite-plugin-jdists",
-    // 强制插件在 transform 阶段运行
-    enforce: "pre",
-    /**
-     * 转换代码
-     * @param {string} code - 源代码
-     * @param {string} id - 文件路径
-     * @returns {string} 处理后的代码
-     */
-    transform(r, t) {
-      if (!o(t))
-        return null;
-      try {
-        const e = {
-          fromString: !0,
-          path: t,
-          ...d
-        };
-        return i && (e.remove = l(i)), n && (e.trigger = l(n)), s && (e.config = s), {
-          code: g.build(r, e),
-          map: null
-        };
-      } catch {
-        return {
-          code: r,
-          map: null
-        };
-      }
-    },
-    /**
-     * 处理热更新
-     * @param {Object} ctx - 热更新上下文
-     */
-    handleHotUpdate(r) {
-      const { file: t, server: e, modules: u } = r;
-      if (o(t))
-        return console.log(`[vite-plugin-jdists] 热更新文件: ${t}`), u;
-    }
-  };
+import jdists from "jdists";
+import { createFilter } from "@rollup/pluginutils";
+function viteJdistsPlugin(n = {}) {
+	let { include: r = /\.(js|ts|jsx|tsx|vue|svelte|astro|md)$/, exclude: i = [], remove: a = ["debug", "test"], trigger: o = ["release"], config: s, ...c } = n, l = createFilter(r, i), u = (e) => Array.isArray(e) ? e.join(",") : e;
+	return {
+		name: "vite-plugin-jdists",
+		enforce: "pre",
+		transform(t, n) {
+			if (!l(n)) return null;
+			try {
+				let r = {
+					fromString: !0,
+					path: n,
+					...c
+				};
+				return a && (r.remove = u(a)), o && (r.trigger = u(o)), s && (r.config = s), {
+					code: jdists.build(t, r),
+					map: null
+				};
+			} catch {
+				return {
+					code: t,
+					map: null
+				};
+			}
+		},
+		handleHotUpdate(e) {
+			let { file: t, server: n, modules: r } = e;
+			if (l(t)) return console.log(`[vite-plugin-jdists] 热更新文件: ${t}`), r;
+		}
+	};
 }
-export {
-  j as default
-};
+export { viteJdistsPlugin as default };
